b2665704336e8f03e6e9408d41a230c8f5a56a2c
OmarSD <salgadodiaz_wwe@outlook.com>
2018-10-01 13:35:20 -0500

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -1,13 +1,16 @@
+// Puerto
 process.env.PORT = process.env.PORT || 3000;
 
+// Entorno
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
+// Base de Datos
 let urlDB;
 
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = "mongodb://cafe-user:12345q@ds119853.mlab.com:19853/cafe";
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
